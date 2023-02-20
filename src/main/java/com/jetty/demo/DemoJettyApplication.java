c65248160d29063c042a6172786d612ff3aa03d9
alumnodavidfei@gmail.com <hjCDRfdhu142>
2023-02-20 17:25:45 +0100

    Initial commit

---
+++ src/main/java/com/jetty/demo/DemoJettyApplication.java
@@ -0,0 +1,13 @@
+package com.jetty.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJettyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jetty/demo/DemoJettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetty.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJettyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
